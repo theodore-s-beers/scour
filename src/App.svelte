476b4aba303a4b74d312f6548e38a26f86b1e9a7
Theodore Beers <theo.beers@fu-berlin.de>
2020-08-29 14:44:35 +0200

    Change to source code repo

---
+++ src/App.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import Info from "./Info.svelte";
+  import { cleanText, copyText } from "./utils.svelte";
+
+  let info = false;
+
+  let diacsCheck = true;
+  let extrasCheck = false;
+
+  let origTextInput = "";
+
+  let cleanTextField: HTMLTextAreaElement;
+  $: cleanTextOutput = cleanText(origTextInput, diacsCheck, extrasCheck);
+</script>
+
+<style>
+  main {
+    max-width: 72ch;
+    padding: 2ch;
+    margin: auto;
+    font-family: "Noto Serif", serif;
+  }
+  button {
+    margin-bottom: 1em;
+    background-color: #007bff;
+    color: #fff;
+    border-radius: 0.25rem;
+    border-color: #007bff;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  }
+  button:hover,
+  button:focus {
+    background-color: #0069d9;
+    border-color: #0062cc;
+  }
+  button:focus {
+    box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
+  }
+  code {
+    font-size: 150%;
+    color: #e83e8c;
+    margin-right: 1em;
+  }
+  h1 {
+    margin-top: -0.3em;
+  }
+  textarea {
+    width: 100%;
+    border-radius: 0.25rem;
+  }
+  .checkboxLabel {
+    display: inline-block;
+    margin-right: 1em;
+  }
+  .count {
+    margin-bottom: 0.4em;
+  }
+  .textareaLabel {
+    margin-bottom: 0.4em;
+  }
+  #checkboxes {
+    display: flex;
+    flex-flow: row wrap;
+    margin-bottom: 0.6em;
+  }
+  #counts {
+    display: flex;
+    flex-flow: row wrap;
+  }
+  #infoButton {
+    display: flex;
+    justify-content: flex-end;
+    font-size: small;
+  }
+</style>
+
+<svelte:head>
+
+  <meta
+    name="description"
+    content="Clean text to prepare for pasting into a document." />
+
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital@0;1&display=swap"
+    rel="stylesheet" />
+
+  <title>Clean text for pasting</title>
+
+</svelte:head>
+
+<main>
+
+  <div id="infoButton">
+    <button on:click={() => (info = !info)}>
+      {#if !info}Show info{:else}Return to utility{/if}
+    </button>
+  </div>
+
+  <h1>Clean text for pasting</h1>
+
+  {#if !info}
+    <div in:fade>
+
+      <div id="checkboxes">
+        <div>
+          <input type="checkbox" id="diacsCheck" bind:checked={diacsCheck} />
+          <label for="diacsCheck" class="checkboxLabel">Fix ṡ, ż</label>
+        </div>
+
+        <div>
+          <input type="checkbox" id="extrasCheck" bind:checked={extrasCheck} />
+          <label for="extrasCheck" class="checkboxLabel">Extras</label>
+        </div>
+      </div>
+
+      <div>
+        <label for="origTextInput" class="textareaLabel">Input:</label>
+        <textarea bind:value={origTextInput} id="origTextInput" rows="4" />
+      </div>
+
+      <div>
+        <button on:click={() => (origTextInput = '')}>Clear</button>
+      </div>
+
+      <div>
+        <label for="cleanTextOutput" class="textareaLabel">Output:</label>
+        <textarea
+          bind:this={cleanTextField}
+          bind:value={cleanTextOutput}
+          readonly
+          autocomplete="off"
+          id="cleanTextOutput"
+          rows="4" />
+      </div>
+
+      <div>
+        <button on:click={() => copyText(cleanTextOutput, cleanTextField)}>
+          Copy
+        </button>
+      </div>
+
+      <div id="counts">
+        <div class="count">
+          Characters:
+          <code>{String(cleanTextOutput.length)}</code>
+        </div>
+
+        <div class="count">
+          Words:
+          <code>
+            {cleanTextOutput.length === 0 ? '0' : String(cleanTextOutput
+                    .replace(/\s{2,}/gm, ' ')
+                    .split(/\s/gm).length)}
+          </code>
+        </div>
+
+        <div class="count">
+          Paragraphs:
+          <code>
+            {cleanTextOutput.length === 0 ? '0' : String(cleanTextOutput.split('\n\n').length)}
+          </code>
+        </div>
+      </div>
+
+    </div>
+  {:else}
+    <Info />
+  {/if}
+
+</main>
---
+++ src/Info.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+</script>
+
+<div in:fade>
+
+  <h2>What does this do?</h2>
+
+  <p>
+    By default, this script will normalize the input text to Unicode form NFC;
+    trim outside whitespace; replace two annoying diacritics specific to the
+    romanization of Persian (this can be disabled); remove any remaining
+    combining diacritics (none should be present after NFC normalization);
+    replace any unusual horizontal space character (including tab) with a normal
+    space; reduce any instance of multiple spaces to one; remove any space
+    before a line break; reduce any instance of multiple empty lines to one; and
+    replace any non-breaking hyphen or figure dash with a normal hyphen.
+  </p>
+
+  <p>
+    With “extras” enabled, this will further replace single or double curly
+    quotes with their straight equivalents; and replace any en or em dash with
+    two or three normal hyphens, respectively.
+  </p>
+
+  <p>
+    Naïve counts of characters, words, and paragraphs (where separated by an
+    empty line) are given for the cleaned output text.
+  </p>
+
+  <p>Other features may be added over time, as they occur to me.</p>
+
+  <hr />
+
+  <p>
+    A plague year diversion of
+    <a href="https://www.theobeers.com/" rel="noopener">Theo Beers</a>
+    <br />
+    <em>Last updated 28 Aug. 2020</em>
+  </p>
+
+</div>
