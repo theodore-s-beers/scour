9162556f98eabd2a3b5afd8459f1456da80748c0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-05-17 14:24:40 +0200

    Remove animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-	import { cleanText, copyText } from '$lib/utils.svelte';
-	import { browser } from '$app/env';
 	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+	import { cleanText, copyText } from '$lib/utils.svelte';
 
 	let diacsCheck: boolean;
 	let extrasCheck: boolean;
@@ -32,123 +31,121 @@
 	<title>Clean text for pasting</title>
 </svelte:head>
 
-<div in:fly={{ x: -100, delay: 200 }} out:fly={{ x: -100, duration: 200 }}>
-	<div class="flex space-x-6 mb-3">
-		<div class="flex space-x-2 items-center">
-			<input
-				type="checkbox"
-				bind:checked={diacsCheck}
-				on:change={() => {
-					if (browser) {
-						localStorage.setItem('diacsCheck', diacsCheck.toString());
-					}
-				}}
-				class="w-5 h-5 md:w-4 md:h-4"
-				id="diacs-check"
-			/><label for="diacs-check">Fix ṡ, ż</label>
-		</div>
-
-		<div class="flex space-x-2 items-center">
-			<input
-				type="checkbox"
-				bind:checked={extrasCheck}
-				on:change={() => {
-					if (browser) {
-						localStorage.setItem('extrasCheck', extrasCheck.toString());
-					}
-				}}
-				class="w-5 h-5 md:w-4 md:h-4"
-				id="extras-check"
-			/><label for="extras-check">Extras</label>
-		</div>
-
-		<div class="flex space-x-2 items-center">
-			<input
-				type="checkbox"
-				bind:checked={lowercaseCheck}
-				on:change={() => {
-					if (browser) {
-						localStorage.setItem('lowercaseCheck', lowercaseCheck.toString());
-					}
-				}}
-				class="w-5 h-5 md:w-4 md:h-4"
-				id="lowercase-check"
-			/><label for="lowercase-check">Lowercase</label>
-		</div>
+<div class="flex space-x-6 mb-3">
+	<div class="flex space-x-2 items-center">
+		<input
+			type="checkbox"
+			bind:checked={diacsCheck}
+			on:change={() => {
+				if (browser) {
+					localStorage.setItem('diacsCheck', diacsCheck.toString());
+				}
+			}}
+			class="w-5 h-5 md:w-4 md:h-4"
+			id="diacs-check"
+		/><label for="diacs-check">Fix ṡ, ż</label>
 	</div>
 
-	<div class="mb-1"><label for="orig-text-input">Input:</label></div>
-	<div class="mb-1">
-		<textarea
-			bind:value={origTextInput}
-			on:change={setInput}
-			rows="8"
-			class="border border-gray-300 rounded w-full p-2 bg-gray-50"
-			id="orig-text-input"
-		/>
+	<div class="flex space-x-2 items-center">
+		<input
+			type="checkbox"
+			bind:checked={extrasCheck}
+			on:change={() => {
+				if (browser) {
+					localStorage.setItem('extrasCheck', extrasCheck.toString());
+				}
+			}}
+			class="w-5 h-5 md:w-4 md:h-4"
+			id="extras-check"
+		/><label for="extras-check">Extras</label>
 	</div>
 
-	<div class="mb-3 flex">
-		<div class="mr-4">
-			<button
-				on:click={() => {
-					origTextInput = '';
-					setInput();
-				}}
-				class="bg-blue-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Clear</button
-			>
-		</div>
-
-		<div>
-			<button
-				on:click={() => {
-					origTextInput = cleanTextOutput;
-					setInput();
-				}}
-				class="bg-teal-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Cycle</button
-			>
-		</div>
+	<div class="flex space-x-2 items-center">
+		<input
+			type="checkbox"
+			bind:checked={lowercaseCheck}
+			on:change={() => {
+				if (browser) {
+					localStorage.setItem('lowercaseCheck', lowercaseCheck.toString());
+				}
+			}}
+			class="w-5 h-5 md:w-4 md:h-4"
+			id="lowercase-check"
+		/><label for="lowercase-check">Lowercase</label>
 	</div>
+</div>
 
-	<div class="mb-1"><label for="clean-text-output">Output:</label></div>
-	<div class="mb-1">
-		<textarea
-			bind:this={cleanTextField}
-			bind:value={cleanTextOutput}
-			readonly
-			autocomplete="off"
-			rows="8"
-			class="border border-gray-300 w-full rounded p-2 bg-gray-50"
-			id="clean-text-output"
-		/>
-	</div>
+<div class="mb-1"><label for="orig-text-input">Input:</label></div>
+<div class="mb-1">
+	<textarea
+		bind:value={origTextInput}
+		on:change={setInput}
+		rows="8"
+		class="border border-gray-300 rounded w-full p-2 bg-gray-50"
+		id="orig-text-input"
+	/>
+</div>
 
-	<div class="mb-2">
+<div class="mb-3 flex">
+	<div class="mr-4">
 		<button
-			on:click={() => copyText(cleanTextField, cleanTextOutput)}
-			class="bg-blue-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Copy</button
+			on:click={() => {
+				origTextInput = '';
+				setInput();
+			}}
+			class="bg-blue-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Clear</button
 		>
 	</div>
 
-	<div class="flex flex-wrap">
-		<div class="flex space-x-2 mr-4 items-baseline">
-			<span>Characters:</span><span class="font-mono text-pink-600 text-lg"
-				>{String(cleanTextOutput.length)}</span
-			>
-		</div>
+	<div>
+		<button
+			on:click={() => {
+				origTextInput = cleanTextOutput;
+				setInput();
+			}}
+			class="bg-teal-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Cycle</button
+		>
+	</div>
+</div>
 
-		<div class="flex space-x-2 mr-4 items-baseline">
-			<span>Words:</span><span class="font-mono text-pink-600 text-lg"
-				>{cleanTextOutput.length === 0
-					? '0'
-					: String(cleanTextOutput.replace(/\s{2,}/g, ' ').split(/\s/g).length)}</span
-			>
-		</div>
+<div class="mb-1"><label for="clean-text-output">Output:</label></div>
+<div class="mb-1">
+	<textarea
+		bind:this={cleanTextField}
+		bind:value={cleanTextOutput}
+		readonly
+		autocomplete="off"
+		rows="8"
+		class="border border-gray-300 w-full rounded p-2 bg-gray-50"
+		id="clean-text-output"
+	/>
+</div>
 
-		<div class="flex space-x-2 items-baseline">
-			<span>Paragraphs:</span><span class="font-mono text-pink-600 text-lg"
-				>{cleanTextOutput.length === 0 ? '0' : String(cleanTextOutput.split('\n\n').length)}</span
-			>
-		</div>
+<div class="mb-2">
+	<button
+		on:click={() => copyText(cleanTextField, cleanTextOutput)}
+		class="bg-blue-600 text-gray-50 px-2 py-1 rounded text-lg md:text-base">Copy</button
+	>
+</div>
+
+<div class="flex flex-wrap">
+	<div class="flex space-x-2 mr-4 items-baseline">
+		<span>Characters:</span><span class="font-mono text-pink-600 text-lg"
+			>{String(cleanTextOutput.length)}</span
+		>
+	</div>
+
+	<div class="flex space-x-2 mr-4 items-baseline">
+		<span>Words:</span><span class="font-mono text-pink-600 text-lg"
+			>{cleanTextOutput.length === 0
+				? '0'
+				: String(cleanTextOutput.replace(/\s{2,}/g, ' ').split(/\s/g).length)}</span
+		>
+	</div>
+
+	<div class="flex space-x-2 items-baseline">
+		<span>Paragraphs:</span><span class="font-mono text-pink-600 text-lg"
+			>{cleanTextOutput.length === 0 ? '0' : String(cleanTextOutput.split('\n\n').length)}</span
+		>
 	</div>
 </div>
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,65 +1,59 @@
-<script lang="ts">
-	import { fly } from 'svelte/transition';
-</script>
-
 <svelte:head>
 	<title>Clean text for pasting – Info</title>
 </svelte:head>
 
-<div in:fly={{ x: 100, delay: 200 }} out:fly={{ x: 100, duration: 200 }}>
-	<h2 class="text-2xl mb-4">What does this do?</h2>
+<h2 class="text-2xl mb-4">What does this do?</h2>
 
-	<div class="space-y-3">
-		<p>
-			By default, this script will normalize the input text to Unicode form NFC; trim outside
-			whitespace; replace two annoying diacritics specific to the romanization of Persian (this can
-			be disabled); remove any remaining combining diacritics (none should be present after NFC
-			normalization); replace any unusual horizontal space character (including tab) with a normal
-			space; reduce any instance of multiple spaces to one; remove any space before or after a line
-			break; reduce any instance of multiple empty lines to one; and replace any non-breaking hyphen
-			or figure dash with a normal hyphen.
-		</p>
-		<p>
-			With “extras” enabled, this will further replace single or double curly quotes with their
-			straight equivalents; and replace any en or em dash with two or three normal hyphens,
-			respectively.
-		</p>
-		<p>
-			There is also an option to convert all letters in the input text to lowercase. (The JavaScript
-			method in question will attempt to do this in a locale-sensitive manner.)
-		</p>
-		<p>
-			Naïve counts of characters, words, and paragraphs (where separated by an empty line) are given
-			for the cleaned output text.
-		</p>
-		<p>
-			User input is persisted to browser <a
-				href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage"
-				rel="noopener"
-				class="text-blue-600 hover:underline">localStorage</a
-			>. Don’t worry—there’s no backend.
-		</p>
-		<p>
-			Other features may be added over time, as they occur to me. In any case, the source files are
-			available
-			<a
-				href="https://github.com/theodore-s-beers/scour"
-				rel="noopener"
-				class="text-blue-600 hover:underline"
-			>
-				on GitHub
-			</a>
-			under the MIT License.
-		</p>
-	</div>
-
-	<hr class="mt-4 mb-2" />
-
+<div class="space-y-3">
 	<p>
-		A plague year diversion of <a
-			href="https://www.theobeers.com/"
+		By default, this script will normalize the input text to Unicode form NFC; trim outside
+		whitespace; replace two annoying diacritics specific to the romanization of Persian (this can be
+		disabled); remove any remaining combining diacritics (none should be present after NFC
+		normalization); replace any unusual horizontal space character (including tab) with a normal
+		space; reduce any instance of multiple spaces to one; remove any space before or after a line
+		break; reduce any instance of multiple empty lines to one; and replace any non-breaking hyphen
+		or figure dash with a normal hyphen.
+	</p>
+	<p>
+		With “extras” enabled, this will further replace single or double curly quotes with their
+		straight equivalents; and replace any en or em dash with two or three normal hyphens,
+		respectively.
+	</p>
+	<p>
+		There is also an option to convert all letters in the input text to lowercase. (The JavaScript
+		method in question will attempt to do this in a locale-sensitive manner.)
+	</p>
+	<p>
+		Naïve counts of characters, words, and paragraphs (where separated by an empty line) are given
+		for the cleaned output text.
+	</p>
+	<p>
+		User input is persisted to browser <a
+			href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage"
 			rel="noopener"
-			class="text-blue-600 hover:underline">Theo Beers</a
-		><br /><em>Last updated 5 Apr. 2022</em>
+			class="text-blue-600 hover:underline">localStorage</a
+		>. Don’t worry—there’s no backend.
+	</p>
+	<p>
+		Other features may be added over time, as they occur to me. In any case, the source files are
+		available
+		<a
+			href="https://github.com/theodore-s-beers/scour"
+			rel="noopener"
+			class="text-blue-600 hover:underline"
+		>
+			on GitHub
+		</a>
+		under the MIT License.
 	</p>
 </div>
+
+<hr class="mt-4 mb-2" />
+
+<p>
+	A plague year diversion of <a
+		href="https://www.theobeers.com/"
+		rel="noopener"
+		class="text-blue-600 hover:underline">Theo Beers</a
+	><br /><em>Last updated 5 Apr. 2022</em>
+</p>
