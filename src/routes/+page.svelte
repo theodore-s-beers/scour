fac1b272183345a9ad1c605b144d8d0ebce964af
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-08-29 22:19:04 +0200

    Bump deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { browser } from '$app/env';
+	import { browser } from '$app/environment';
 	import { cleanText, copyText } from '$lib/utils.svelte';
 
 	let diacsCheck: boolean;
