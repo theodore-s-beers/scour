1f3695042b23669a81fdbe812a14f56e2408e7a0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-01-06 23:25:58 +0100

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
