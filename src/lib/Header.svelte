6f1d0397429e6abaed94b0bd1d25b9e375cdb2ea
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-09-28 19:50:21 -0400

    Resolve relative links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import { page } from "$app/state";
+	import { resolve } from "$app/paths";
 
 	let info = $derived(page.url.pathname === "/info");
 </script>
 
 <div class="mb-3 flex justify-end">
 	{#if info}
-		<a href="/" class="rounded bg-blue-600 px-2 py-1 text-lg text-gray-50 md:text-base"
+		<a href={resolve("/")} class="rounded bg-blue-600 px-2 py-1 text-lg text-gray-50 md:text-base"
 			>Return to app</a
 		>
 	{:else}
-		<a href="/info" class="rounded bg-blue-600 px-2 py-1 text-lg text-gray-50 md:text-base"
-			>Show info</a
+		<a
+			href={resolve("/info")}
+			class="rounded bg-blue-600 px-2 py-1 text-lg text-gray-50 md:text-base">Show info</a
 		>
 	{/if}
 </div>
